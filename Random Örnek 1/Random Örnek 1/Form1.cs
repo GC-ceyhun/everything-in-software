5a610cf071b277f7b612f64aa94dc58bde0b5c14
Gökay Ceyhun Şanlıoğlu <90559902+GC-ceyhun@users.noreply.github.com>
2023-03-29 17:45:14 +0300

    Add files via upload

---
+++ Random Örnek 1/Random Örnek 1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Örnek_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color[] renkler = new Color[8] { Color.Red, Color.Yellow, Color.Black, Color.Blue,
+                                             Color.White, Color.Purple, Color.Orange, Color.Brown };
+            Random x = new Random();
+            int dizi_elemanı = x.Next(0, 7);
+            this.BackColor = renkler[dizi_elemanı];
+        }
+    }
+}
